c857b3fc1eba64bcfbb37ba30738e781f0d4477c
ebestford <elizgracbest@gmail.com>
2024-10-02 21:09:08 -0400

    working on bonus

--- scriptz/data analyst job questions.sql
+++ scriptz/data analyst job questions.sql
@@ -16,7 +16,7 @@ LIMIT 10;
 --Find how many postings in TN
 SELECT COUNT(location)
 FROM data_analyst_jobs 
-WHERE location = 'TN';
+WHERE location = 'TN'
 --21
 
 --Find how many postings in KY
@@ -63,7 +63,67 @@ WHERE location= 'CA';
 --230
 
 9--Find the name of each company and its average star rating for all companies that have more than 5000 reviews across all locations. How many companies are there with more that 5000 reviews across all locations?
-SELECT company 
+SELECT company, star_rating
 FROM data_analyst_jobs
-WHERE av_rating > 5000;
---INCOMPLETE
+WHERE review_count > 5000;
+--Don't believe this is complete, stuck on how to find average rating in this
+--PART B
+SELECT COUNT (company)
+FROM data_analyst_jobs
+WHERE review_count > 5000;
+--184
+
+10--Add the code to order the query in #9 from highest to lowest average star rating. Which company with more than 5000 reviews across all locations in the dataset has the highest star rating? What is that rating?
+SELECT company, star_rating
+FROM data_analyst_jobs
+WHERE review_count > 5000
+ORDER BY star_rating DESC, review_count DESC;
+--Kaiser Permanente
+--Unsure if this is correct, back on the average star rating question as before
+
+11--Find all the job titles that contain the word ‘Analyst’. How many different job titles are there? 
+SELECT title
+FROM data_analyst_jobs
+WHERE title LIKE 'Analyst%';
+
+SELECT COUNT(title)
+FROM data_analyst_jobs
+WHERE title LIKE 'Analyst%';
+--12
+
+12--How many different job titles do not contain either the word ‘Analyst’ or the word ‘Analytics’? What word do these positions have in common?
+SELECT COUNT(title)
+FROM data_analyst_jobs
+WHERE title NOT LIKE 'Analyst%'
+	AND title NOT LIKE 'Analytics%';
+
+SELECT title
+FROM data_analyst_jobs
+WHERE title NOT LIKE '%Analyst';
+--only giving back analyst and analytics results..
+
+	-- BONUS--
+-- You want to understand which jobs requiring SQL are hard to fill. Find the number of jobs by industry (domain) that require SQL and have been posted longer than 3 weeks. 
+--  - Disregard any postings where the domain is NULL. 
+--  - Order your results so that the domain with the greatest number of `hard to fill` jobs is at the top. 
+--   - Which three industries are in the top 3 on this list? How many jobs have been listed for more than 3 weeks for each of the top 3?
+
+SELECT COUNT(domain)
+FROM data_analyst_jobs
+WHERE days_since_posting > 21
+	AND skill = 'SQL';
+--11 
+--Doesnt seem right
+SELECT domain, days_since_posting, skill
+FROM data_analyst_jobs
+WHERE days_since_posting > 21
+	AND skill = 'SQL';
+--15
+--Seems more right?
+SELECT *
+FROM data_analyst_jobs
+WHERE days_since_posting > 21
+	AND skill='SQL';
+--Double checking data
+
+SELECT domain

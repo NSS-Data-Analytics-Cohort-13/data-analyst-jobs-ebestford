f092131fcc4f376c044c176f6867d6d1c43abf1f
ebestford <elizgracbest@gmail.com>
2024-10-05 12:48:13 -0400

    finished

--- scriptz/data analyst job questions.sql
+++ scriptz/data analyst job questions.sql
@@ -11,7 +11,7 @@ FROM data_analyst_jobs;
 SELECT *
 FROM data_analyst_jobs
 LIMIT 10;
---Company associated with 10th row, XTO Land Data Analyst
+--Company associated with 10th row, ExxonMobil
 
 --Find how many postings in TN
 SELECT COUNT(location)
@@ -35,21 +35,23 @@ WHERE location = 'TN'
 4--How many postings in TN have star ratings above 4
 SELECT COUNT (star_rating)
 FROM data_analyst_jobs
-WHERE star_rating >4;
+WHERE location = 'TN' AND star_rating >4;
 --416
 
 
 5-- Find how many postings had reviews between 500 and 1000
-SELECT COUNT (review_count)
+SELECT COUNT (*)
 FROM data_analyst_jobs
 WHERE review_count BETWEEN 500 AND 1000;
 --151 postings
 
 6--Show the average star rating for companies in each state. The output should show the state as `state` and the average rating for the state as `avg_rating`. Which state shows the highest average rating?
-SELECT company, location AS state, AVG (star_rating) AS av_rating
+SELECT location AS state, ROUND (AVG (star_rating),1) AS av_rating
 FROM data_analyst_jobs
-GROUP BY location, company;
---INCOMPLETE-I think I have to use DESC or eliminate null values for answer with highest rating
+WHERE star_rating IS NOT NULL
+GROUP BY location
+ORDER BY av_rating DESC;
+--NE
 
 7--Select unique job titles from the data_analyst_jobs table. How many are there?
 SELECT COUNT (DISTINCT title)
@@ -63,43 +65,52 @@ WHERE location= 'CA';
 --230
 
 9--Find the name of each company and its average star rating for all companies that have more than 5000 reviews across all locations. How many companies are there with more that 5000 reviews across all locations?
-SELECT company, star_rating
+SELECT company, AVG (star_rating) AS av_rating
 FROM data_analyst_jobs
-WHERE review_count > 5000;
---Don't believe this is complete, stuck on how to find average rating in this
---PART B
-SELECT COUNT (company)
+WHERE company IS NOT NULL 
+GROUP BY company
+HAVING MIN (review_count) > 5000;
+--40
+
+
+SELECT COUNT (company), AVG (star_rating) AS av_rating
 FROM data_analyst_jobs
-WHERE review_count > 5000;
---184
+WHERE company IS NOT NULL 
+GROUP BY company
+HAVING MIN (review_count) > 5000;
+--40
 
 10--Add the code to order the query in #9 from highest to lowest average star rating. Which company with more than 5000 reviews across all locations in the dataset has the highest star rating? What is that rating?
-SELECT company, star_rating
+SELECT company, ROUND(AVG (star_rating),2) AS avg_rating
 FROM data_analyst_jobs
-WHERE review_count > 5000
-ORDER BY star_rating DESC, review_count DESC;
---Kaiser Permanente
---Unsure if this is correct, back on the average star rating question as before
+WHERE review_count IS NOT NULL
+	AND company IS NOT NULL 
+GROUP BY company 
+HAVING MIN (review_count) > 5000
+ORDER BY avg_rating DESC;
+
+--Tie for 6 companies
 
 11--Find all the job titles that contain the word ‘Analyst’. How many different job titles are there? 
-SELECT title
+SELECT DISTINCT title AS job_title
 FROM data_analyst_jobs
-WHERE title LIKE 'Analyst%';
+WHERE title ILIKE '%Analyst%';
+--774
 
-SELECT COUNT(title)
+SELECT COUNT (DISTINCT title) AS job_title
 FROM data_analyst_jobs
-WHERE title LIKE 'Analyst%';
---12
+WHERE title ILIKE '%Analyst%';
+--774
 
 12--How many different job titles do not contain either the word ‘Analyst’ or the word ‘Analytics’? What word do these positions have in common?
-SELECT COUNT(title)
+SELECT DISTINCT title AS job_title
 FROM data_analyst_jobs
-WHERE title NOT LIKE 'Analyst%'
-	AND title NOT LIKE 'Analytics%';
+WHERE title NOT ILIKE '%Analyst%'
+	AND title NOT ILIKE '%Analytics%';
 
 SELECT title
 FROM data_analyst_jobs
-WHERE title NOT LIKE '%Analyst';
+WHERE title NOT ILIKE '%Analyst%';
 --only giving back analyst and analytics results..
 
 	-- BONUS--
@@ -108,22 +119,12 @@ WHERE title NOT LIKE '%Analyst';
 --  - Order your results so that the domain with the greatest number of `hard to fill` jobs is at the top. 
 --   - Which three industries are in the top 3 on this list? How many jobs have been listed for more than 3 weeks for each of the top 3?
 
-SELECT COUNT(domain)
-FROM data_analyst_jobs
-WHERE days_since_posting > 21
-	AND skill = 'SQL';
---11 
---Doesnt seem right
-SELECT domain, days_since_posting, skill
-FROM data_analyst_jobs
-WHERE days_since_posting > 21
-	AND skill = 'SQL';
---15
---Seems more right?
-SELECT *
-FROM data_analyst_jobs
-WHERE days_since_posting > 21
-	AND skill='SQL';
---Double checking data
+SELECT DISTINCT domain AS industry, COUNT(skill) AS sql_skill
+ FROM data_analyst_jobs
+ WHERE days_since_posting >21
+ 	AND domain IS NOT NULL
+	AND skill iLIKE '%sql%'
+GROUP BY industry
+ORDER BY sql_skill DESC, industry;
 
-SELECT domain
+
